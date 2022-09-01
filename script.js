54fb2f0b38eb8154f7a0a9f911a38ad1dbf79457
TomasArancibia <tomas.arancibia@hotmail.com>
2022-09-01 06:14:45 -0400

    avance esqueleto chart

--- script.js
+++ script.js
@@ -72,3 +72,34 @@ function validate() {
     }
     return isValid;
 }
+
+
+
+const data = {
+    'React': 185134,
+    'Vue': 195514,
+    'Angular': 80460,
+    'Svelte': 57022,
+    'Ember.js': 22165,
+    'Backbone.js': 27862
+};
+
+const ctx = document.getElementById('myChart').getContext('2d');
+
+const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: Object.keys(data),
+        datasets: [{
+            label: 'Number of GitHub Stars',
+            data: Object.values(data),
+        }, ],
+    },
+    options: {
+        backgroundColor: [
+            'red'
+        ],
+        borderWidth: 2,
+        borderColor: 'black'
+    }
+});
